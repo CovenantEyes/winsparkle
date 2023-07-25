5aec04ee178eddc5cb55e34b2b5fbc05463c1bb1
Jared Burkeen <jared.burkeen@covenanteyes.com>
2023-07-25 09:07:44 -0400

    fixup(): Add comments for IsWindowsService function

--- src/service.cpp
+++ src/service.cpp
@@ -1,5 +1,7 @@
 #include "service.h"
 
+// This function borrowed from here: https://www.appsloveworld.com/cplus/100/230/how-to-check-that-the-current-process-is-running-as-windows-service-using-winapi
+// This solution works because all Windows services run in Session 0.
 BOOL IsWindowsService()
 {
     DWORD sessionId = 0;
